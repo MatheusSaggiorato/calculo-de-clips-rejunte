3238650f84cc1340d7503f5ede2782cb39c1ec21
MatheusSaggiorato <matheussaggiorato@gmail.com>
2023-04-19 15:27:35 -0300

    first commit

---
+++ assets/scripts/script.js
@@ -0,0 +1,11 @@
+(function limitarInputs() {
+    var inputs = document.querySelectorAll("input[type='number']");
+
+    for (var i = 0; i < inputs.length; i++) {
+        inputs[i].oninput = function () {
+            if (this.value.length > 3) {
+                this.value = this.value.slice(0, 3);
+            }
+        };
+    }
+})();
