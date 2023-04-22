0457be93c1d75696ae95f560e6f36784154ba318
MatheusSaggiorato <matheussaggiorato@gmail.com>
2023-04-22 17:44:25 -0300

    projeto finalizado - entregue

--- assets/scripts/CalcArgamassa.js
+++ assets/scripts/CalcArgamassa.js
@@ -1,49 +1,52 @@
-//inicio do script para calculo de argamassa a rejunte
-const ladoAInputArg = document.getElementById("lado-a-argamassa");
-const ladoBInputArg = document.getElementById("lado-b-argamassa");
-const areaTotalInputArg = document.getElementById("area-total-argamassa");
-const espessuraJunta = document.getElementById("espessura-junta");
-const espessuraRevestimento = document.getElementById("espessura-revestimento");
+let coeficienteRejuntamento = 1.58;
 
-let coeficienteRejunte = 1.58;
-
-function atualizarCoeficente() {
+function atualizarCoeficenteRejuntamento() {
 
     const rejunteSelecionado = document.getElementById("dropdown-tipo-rejunte").value;
 
     switch (rejunteSelecionado) {
         case "opcao1":
-            coeficienteRejunte = 1.58;
+            coeficienteRejuntamento = 1.58;
             break;
         case "opcao2":
-            coeficienteRejunte = 1.75;
+            coeficienteRejuntamento = 1.75;
             break;
         case "opcao3":
-            coeficienteRejunte = 1.32;
+            coeficienteRejuntamento = 1.32;
             break;
         default:
-            coeficienteRejunte = 1.58;
+            coeficienteRejuntamento = 1.58;
     }
-}
+};
 
-function calcArgamassaRejunte() {
-    if (ladoAInputArg.value === "" ||
-        ladoBInputArg.value === '' ||
-        areaTotalInputArg.value === '' ||
-        espessuraJunta.value === '' ||
-        espessuraRevestimento.value === '') {
-        alert('Por favor, preencha todos os campos com valores diferentes de zero');
+function calcularArgamassaRejunte() {
+
+    const ladoAInputArgamassa = document.getElementById("lado-a-argamassa");
+    const ladoBInputArgamassa = document.getElementById("lado-b-argamassa");
+    const areaTotalInputArgamassa = document.getElementById("area-total-argamassa");
+    const espessuraJunta = document.getElementById("espessura-junta");
+    const profundidadeJunta = document.getElementById("espessura-revestimento");
+
+    function validarCampos(...campos) {
+        for (const campo of campos) {
+            if (!campo.value) {
+                alert('Por favor, preencha todos os campos com valores diferentes de zero');
+                return true;
+            }
+        }
+        return false;
+    }
+
+    if (validarCampos(ladoAInputArgamassa, ladoBInputArgamassa, areaTotalInputArgamassa, espessuraJunta, profundidadeJunta)) {
     } else {
 
-        let quantMinArgamassa = areaTotalInputArg.value * 8;
-        let quantMaxArgamassa = areaTotalInputArg.value * 9;
-
-        const rejunteSelecionado = document.getElementById("dropdown-tipo-rejunte").value;
+        const quantMinArgamassa = areaTotalInputArgamassa.value * 8;
+        const quantMaxArgamassa = areaTotalInputArgamassa.value * 9;
 
         //cálculo do consumo dos rejuntes
-        let ConsumoRejuntePorM2 = (ladoAInputArg.value * 10 + ladoBInputArg.value * 10) * espessuraRevestimento.value * espessuraJunta.value * coeficienteRejunte / ((ladoAInputArg.value * 10) * (ladoBInputArg.value * 10));
-        let quantMinRejunte = (areaTotalInputArg.value * ConsumoRejuntePorM2).toFixed(3);
-        let quantMaxRejunte = (quantMinRejunte * 1.10).toFixed(3);
+        const consumoRejuntePorM2 = (ladoAInputArgamassa.value * 10 + ladoBInputArgamassa.value * 10) * profundidadeJunta.value * espessuraJunta.value * coeficienteRejuntamento / ((ladoAInputArgamassa.value * 10) * (ladoBInputArgamassa.value * 10));
+        const quantMinRejunte = (areaTotalInputArgamassa.value * consumoRejuntePorM2).toFixed(3);
+        const quantMaxRejunte = (quantMinRejunte * 1.10).toFixed(3);
 
         // Atualize o conteúdo dos elementos <p>
         document.querySelector("#consumo-min-rejunte").textContent = `${quantMinRejunte}kg = ${Math.ceil(quantMinRejunte / 1)} saco(s) de 1kg`;
--- assets/scripts/CalcClips.js
+++ assets/scripts/CalcClips.js
@@ -1,17 +1,18 @@
 //inicio do script para calculo de clips e cunhas
-let ladoAInput = document.getElementById("lado-a");
-let ladoBInput = document.getElementById("lado-b");
-let areaTotalInput = document.getElementById("area-total");
-let assentamentoDiarioInput = document.getElementById("assentamento-diario");
+const ladoAInputClips = document.getElementById("lado-a");
+const ladoBInputClips = document.getElementById("lado-b");
+const areaTotalInputClips = document.getElementById("area-total");
+const assentamentoDiarioInputClips = document.getElementById("assentamento-diario");
+
 let clipsRecommendedLadoA, clipsRecommendedLadoB;
 
 // Adicionar um ouvinte de eventos oninput aos elementos de entrada
-ladoAInput.addEventListener('input', atualizarRecomendacoes);
-ladoBInput.addEventListener('input', atualizarRecomendacoes);
+ladoAInputClips.addEventListener('input', atualizarRecomendacoes);
+ladoBInputClips.addEventListener('input', atualizarRecomendacoes);
 
 function atualizarRecomendacoes() {
-  const ladoAValue = parseFloat(ladoAInput.value) || 0;
-  const ladoBValue = parseFloat(ladoBInput.value) || 0;
+  const ladoAValue = parseFloat(ladoAInputClips.value) || 0;
+  const ladoBValue = parseFloat(ladoBInputClips.value) || 0;
 
   function calcularClipsRecomendados(valor) {
     if (valor <= 10) {
@@ -40,24 +41,28 @@ function atualizarRecomendacoes() {
   document.querySelector("#clip-recomended-lado-b").textContent = clipsRecommendedLadoB;
 }
 
+function validarCampos(...campos) {
+  for (const campo of campos) {
+    if (!campo.value) {
+      alert('Por favor, preencha todos os campos com valores diferentes de zero');
+      return true;
+    }
+  }
+  return false;
+}
+
 function calcClipsAndCunhas() {
 
-  let ladoAValue = parseFloat(ladoAInput.value) || 0;
-  let ladoBValue = parseFloat(ladoBInput.value) || 0;
-  let areaTotalValue = parseFloat(areaTotalInput.value) || 0;
-  let assentamentoDiarioValue = parseFloat(assentamentoDiarioInput.value) || 0;
-
-  if (ladoAValue === 0 || ladoBValue === 0 || areaTotalValue === 0 || assentamentoDiarioValue === 0) {
-    alert('Por favor, preencha todos os campos com valores diferentes de zero');
+  if (validarCampos(ladoAInputClips, ladoBInputClips, areaTotalInputClips, assentamentoDiarioInputClips)) {
   } else {
 
     // Implemente seu cálculo aqui
-    let clipsPorRevestimento = clipsRecommendedLadoA * 2 + clipsRecommendedLadoB * 2;
-    let areaDeUmRevestimento = ladoAInput.value * ladoBInput.value / 10000;
-    let quantidadeDeRevestimentos = areaTotalValue / areaDeUmRevestimento;
+    const clipsPorRevestimento = clipsRecommendedLadoA * 2 + clipsRecommendedLadoB * 2;
+    const areaDeUmRevestimento = ladoAInputClips.value * ladoBInputClips.value / 10000;
+    const quantidadeDeRevestimentos = areaTotalInputClips.value / areaDeUmRevestimento;
 
-    let quantidadeClips = Math.round(clipsPorRevestimento * quantidadeDeRevestimentos / 2);
-    let quantidadeCunhas = Math.min(quantidadeClips, Math.ceil((assentamentoDiarioInput.value / areaDeUmRevestimento * clipsPorRevestimento) / 2));
+    const quantidadeClips = Math.round(clipsPorRevestimento * quantidadeDeRevestimentos / 2);
+    const quantidadeCunhas = Math.min(quantidadeClips, Math.ceil((assentamentoDiarioInputClips.value / areaDeUmRevestimento * clipsPorRevestimento) / 2));
 
     // Atualize o conteúdo dos elementos <p>
     document.querySelector("#quantidade-clips").textContent = quantidadeClips;
